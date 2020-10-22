f35863bd2cd9a1dac24df52093757571c906e68b
itsMimao <qiaoxiaoxi621@gmail.com>
2020-10-22 16:34:40 +0800

    compelete proxy nginx start script

---
+++ proxy_server_start.sh
@@ -0,0 +1,43 @@
+HOST_MACHINE_IP=xxx.xxx.xxx.xxx
+origin_config_folder="./conf.origin"
+build_config_folder="./conf.d"
+ABSOLUTE_CONFIG_PATH="your_real_path/proxy_nginx/conf.d"
+NGINX_REPELACE_PATH="/etc/nginx/conf.d"
+NGINX_IMAGE_VERSION="nginx:alpline"
+DOCKER_CONTAINER_NAME="proxyserver"
+
+echo "Start Proxyserver..."
+echo "Host ip is $HOST_MACHINE_IP"
+
+echo "Build config..."
+#删除build文件夹中的所有文件
+if [ ! -d "$build_config_folder" ];then
+  echo "create $build_config_folder"
+  mkdir $build_config_folder
+else
+  echo "$build_config_folder already exists"
+fi
+echo "remove all files in $build_config_folder"
+rm -rf $build_config_folder/*
+
+# 读取所有的nginx子项目配置文件
+originConfigFiles=$(ls $origin_config_folder)
+for originConfig in $originConfigFiles
+do
+  echo "==========>Read config $originConfig and write into $build_config_folder"
+  cp $origin_config_folder/$originConfig $build_config_folder/$originConfig
+  echo "replace \${HOST_MACHINE_IP} to $HOST_MACHINE_IP"
+  sed -i "" "s/\${HOST_MACHINE_IP}/$HOST_MACHINE_IP/g" $build_config_folder/$originConfig
+done
+echo "Build config success!"
+
+result=`docker container ls -a`
+if [[ $result =~ "$DOCKER_CONTAINER_NAME" ]]
+then
+  echo "$DOCKER_CONTAINER_NAME already exists, try restart "
+  docker container restart $DOCKER_CONTAINER_NAME
+else
+  echo "$DOCKER_CONTAINER_NAME not exists, start new one "
+  docker run --name $DOCKER_CONTAINER_NAME -d -p 80:80  -v $ABSOLUTE_CONFIG_PATH:$NGINX_REPELACE_PATH $NGINX_IMAGE_VERSION
+fi
+echo "Done!"
